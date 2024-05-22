19c0ff1f3c0cc87d70ae30630d59e7bfd8fd4fc8
emilianogastaldo <emiliano0303@gmail.com>
2024-05-22 15:21:01 +0200

    step 2 fatto

--- src/zoo/Main.java
+++ src/zoo/Main.java
@@ -4,25 +4,26 @@ public class Main {
     public static void main(String[] args) {
         Animale[] zoo = {new Cane("Pluto"),new Passerotto("Paulie"), new Aquila("Marahute"), new Delfino("Mahito")};
 
-        for (int i = 0; i < zoo.length; i++) {
-            if(zoo[i] instanceof Cane){
-                System.out.println("Nome animale: "+ ((Cane) zoo[i]).nome);
-            } else if (zoo[i] instanceof Passerotto) {
-                System.out.println("Nome animale: "+ ((Passerotto) zoo[i]).nome);
-            } else if (zoo[i] instanceof Aquila){
-                System.out.println("Nome animale: "+ ((Aquila) zoo[i]).nome);
-            } else if (zoo[i] instanceof Delfino) {
-                System.out.println("Nome animale: "+ ((Delfino) zoo[i]).nome);
+        for (Animale animale : zoo) {
+            if (animale instanceof Cane) {
+                System.out.println("Nome animale: " + ((Cane) animale).nome);
+            } else if (animale instanceof Passerotto) {
+                System.out.println("Nome animale: " + ((Passerotto) animale).nome);
+            } else if (animale instanceof Aquila) {
+                System.out.println("Nome animale: " + ((Aquila) animale).nome);
+            } else if (animale instanceof Delfino) {
+                System.out.println("Nome animale: " + ((Delfino) animale).nome);
             }
-            if (zoo[i] instanceof Volare) {
-                faiVolare(((Volare) zoo[i]));
-            }if (zoo[i] instanceof Nuotare) {
-                faiNuotare(((Nuotare) zoo[i]));
+            if (animale instanceof Volare) {
+                faiVolare(((Volare) animale));
+            }
+            if (animale instanceof Nuotare) {
+                faiNuotare(((Nuotare) animale));
             }
 
-            zoo[i].dormi();
-            zoo[i].mangia();
-            zoo[i].verso();
+            animale.dormi();
+            animale.mangia();
+            animale.verso();
             System.out.println("**********************");
         }
 
