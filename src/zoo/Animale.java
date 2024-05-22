4219833b7285378ebcd57b07a18fddf71fbd6527
emilianogastaldo <emiliano0303@gmail.com>
2024-05-22 14:16:14 +0200

    classe astratta Animale

---
+++ src/zoo/Animale.java
@@ -0,0 +1,10 @@
+package zoo;
+
+public abstract class Animale {
+    public void dormi(){
+        System.out.println("Zzz");
+    }
+    public abstract void verso();
+    public abstract void mangia();
+
+}
