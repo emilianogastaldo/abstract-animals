e5d0bb04e6a9eb2b47d9ecae8d7f80230fc687dc
emilianogastaldo <emiliano0303@gmail.com>
2024-05-22 14:30:05 +0200

    classe Delfino

---
+++ src/zoo/Delfino.java
@@ -0,0 +1,18 @@
+package zoo;
+
+public class Delfino extends Animale{
+    private String nome;
+    Delfino(String nome){
+        this.nome = nome;
+    }
+
+    @Override
+    public void verso(){
+        System.out.println("Verso delfino");
+    }
+
+    @Override
+    public void mangia() {
+        System.out.println("Mangio pesci");
+    }
+}
