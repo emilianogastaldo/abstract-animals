ccac9b43e81bd349e8062a0a8fe919f66d8ade14
emilianogastaldo <emiliano0303@gmail.com>
2024-05-22 14:27:34 +0200

    classe Aquila

--- src/zoo/Aquila.java
+++ src/zoo/Aquila.java
@@ -1,6 +1,13 @@
 package zoo;
 
 public class Aquila extends Animale{
+
+    private String nome;
+
+    Aquila(String nome){
+        this.nome = nome;
+    }
+
     @Override
     public void verso() {
         System.out.println("verso dell'aquila");
